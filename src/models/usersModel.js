b64ed108ac473906b2a3d32a280ae5236452bcaf
hnuke <hstuquifatec@gmail.com>
2025-08-24 11:20:58 -0300

    feat: userModel, bcrypt implemented

---
+++ src/models/usersModel.js
@@ -0,0 +1,54 @@
+import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
+
+const SALT_WORK_FACTOR = 8;
+
+const usersSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+}, {timestamps: true});
+
+const Users = mongoose.model('users', usersSchema);
+
+// Middleware
+
+Users.pre('save', () => {
+    const user = this;
+    // only hash the password if it has been modified (or is new)
+    if (!user.isModified('password')) return next();
+
+    // generate a salt
+    bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt){
+        if (err) return next(err);
+        
+        // hash the password along with our new salt
+        bcrypt.hash(user.password, salt, function(err, hash){
+            if (err) return next(err);
+            
+            // override the cleartext password with the hashed one
+            user.password = hash;
+            next();
+        });
+    });
+});
+
+Users.methods.comparePassword = function(candidatePassword, cb){
+    bcrypt.compare(candidatePassword, this.password, function(err, isMatch){
+        if (err) return cb(err);
+        cb(null, isMatch);
+    })
+}
+
+
+export default Users;
