a58f5f7ad5feb016e879797676f636b9957264c4
hnuke <hstuquifatec@gmail.com>
2025-09-03 23:54:18 -0300

    removed infra-docker folder and validateProductMiddleware

--- src/middlewares/validateProductMiddleware.js
+++
@@ -1,9 +0,0 @@
-import ValidateProduct from "../validations/product.validation.js";
-
-export default function validateProductMiddleware(req, res, next){
-    const validation = ValidateProduct(req.body);
-    if (validation) next();
-    else return res.status(400).json({
-        message: "Invalid Product Fields"
-    })
-}
